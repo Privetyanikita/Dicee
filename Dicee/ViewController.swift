02d006ffda42c401988a8b6087d33444c9ac47b4
Privetyanikita <72889842+Privetyanikita@users.noreply.github.com>
2023-05-07 18:57:28 +0200

    add new func

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,25 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var diceeElementImageView1: UIImageView!
-    @IBOutlet weak var button: UIButton!
     @IBOutlet weak var diceeElementImageView2: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        buttonRoll()
+    
+    @IBOutlet weak var pointLabel: UILabel!
+    
+    var pointCount=0
+    let diceArray:[UIImage]=[UIImage(imageLiteralResourceName: "dice1"),UIImage(imageLiteralResourceName: "dice2"),UIImage(imageLiteralResourceName: "dice3"),UIImage(imageLiteralResourceName: "dice4"),UIImage(imageLiteralResourceName: "dice5"),UIImage(imageLiteralResourceName: "dice6")]
+    
+    @IBAction func buttonForRoll(_ sender: UIButton) {
+        diceeElementImageView1.image=diceArray[Int.random(in:0..<diceArray.count)]
+        diceeElementImageView2.image=diceArray[Int.random(in: 0..<diceArray.count)]
+        if diceeElementImageView1.image==diceeElementImageView2.image{
+            pointCount+=1
+            pointLabel.text=String(pointCount)
+        }
     }
-    func viewRandomElement(){
-        var element:[UIImage]=[UIImage(imageLiteralResourceName: "dice1"),UIImage(imageLiteralResourceName: "dice2"),UIImage(imageLiteralResourceName: "dice3"),UIImage(imageLiteralResourceName: "dice4"),UIImage(imageLiteralResourceName: "dice5"),UIImage(imageLiteralResourceName: "dice6")]
-            diceeElementImageView1.image=element[Int.random(in:0..<element.count)]
-            diceeElementImageView2.image=element[Int.random(in: 0..<element.count)]
-    }
-    func buttonRoll(){
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-    }
-    @objc func buttonTapped() {
-        viewRandomElement()
-    }
-
 }
-
